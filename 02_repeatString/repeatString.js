40f978a52192c407eccd73ae641f49e65419b4a2
3BooD-Khalil <3boodkhalilcs@gmail.com>
2025-01-03 08:15:21 +0200

    Finished Exercises 1-4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function (sentence, x) {
+    if (x < 0) return "ERROR";
+    let string = "";
+    for (let i = 0; i < x; i++) {
+      string += sentence;
+    }
+    return string;
+  };
 // Do not edit below this line
 module.exports = repeatString;
